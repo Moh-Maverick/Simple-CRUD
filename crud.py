beb528881452baea4eafb451edced0ac04d264ce
Mohit R <mohit.radkrishnan@gmail.com>
2026-03-05 13:00:19 +0530

    crud functions working

---
+++ crud.py
@@ -0,0 +1,40 @@
+from sqlalchemy.orm import Session
+from models import User
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    name: str
+    email: str
+
+class UserUpdate(BaseModel):
+    name: str
+    email: str
+
+def get_users(db: Session):
+    return db.query(User).all()
+
+def get_user(db: Session, user_id: int):
+    return db.query(User).filter(User.id == user_id).first()
+
+def create_user(db: Session, user: UserCreate):
+    db_user = User(name=user.name, email=user.email)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def update_user(db: Session, user_id: int, user: UserUpdate):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user:
+        db_user.name = user.name
+        db_user.email = user.email
+        db.commit()
+        db.refresh(db_user)
+    return db_user
+
+def delete_user(db: Session, user_id: int):
+    db_user = db.query(User).filter(User.id == user_id).first()
+    if db_user:
+        db.delete(db_user)
+        db.commit()
+    return db_user
